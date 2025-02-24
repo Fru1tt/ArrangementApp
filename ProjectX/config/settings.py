82dcd6f4a3640df7fcff6f56fa9abba2cf60c3a5
Fru1tt <carlgrude@gmail.com>
2025-02-24 17:00:42 +0100

    Changing to confic in settings.py aswell

--- ProjectX/config/settings.py
+++ ProjectX/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "ProjectX.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "ProjectX.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
