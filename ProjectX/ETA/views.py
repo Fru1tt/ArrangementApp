7f0e673af2db06d2e97215b138a0f1d14c0dd056
Fru1tt <carlgrude@gmail.com>
2025-02-24 17:14:18 +0100

    Setting up event management for admin

--- ProjectX/ETA/views.py
+++ ProjectX/ETA/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+def event_list(request):
+    events = Event.objects.all().order_by('date')
+    return render(request, 'ETA/event_list.html', {'events': events})
