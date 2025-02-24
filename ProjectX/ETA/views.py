d0ae20b8a4e7221c1e982f16b95068232bd290d4
Fru1tt <carlgrude@gmail.com>
2025-02-24 17:56:43 +0100

    Login, register page

--- ProjectX/ETA/views.py
+++ ProjectX/ETA/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Event
+from django.contrib.auth.forms import UserCreationForm
 
 def event_list(request):
     events = Event.objects.all().order_by('date')
     return render(request, 'ETA/event_list.html', {'events': events})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # After successful registration, redirect to login
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'ETA/register.html', {'form': form})
