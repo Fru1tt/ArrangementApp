059118127c6b48caf228949472727f41a8cf902a
Fru1tt <carlgrude@gmail.com>
2025-03-24 14:39:30 +0100

    Added profile to admin panel

--- ProjectX/ETA/admin.py
+++ ProjectX/ETA/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Event
+from .models import Profile
 
 admin.site.register(Event)
+admin.site.register(Profile)
 
 
 # Register your models here.
