3bdcf4ce6307debaef883faf54665d0e42c65723
Fru1tt <carlgrude@gmail.com>
2025-03-31 14:50:58 +0200

    attendance model in admin panel

--- ProjectX/ETA/admin.py
+++ ProjectX/ETA/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Event
 from .models import Profile
+from .models import Attendance
 
 admin.site.register(Event)
 admin.site.register(Profile)
-
+admin.site.register(Attendance)
 
 # Register your models here.
