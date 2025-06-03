6e96f4a2c7b5304b6c517d9ce2c67e6b672644da
Fru1tt <carlgrude@gmail.com>
2025-06-03 14:47:43 +0200

    Added Invite request and EventInvite to admin

--- ProjectX/ETA/admin.py
+++ ProjectX/ETA/admin.py
@@ -4,11 +4,15 @@ from .models import Profile
 from .models import Attendance
 from .models import Tag
 from .models import TagCategory
+from .models import InviteRequest
+from .models import EventInvite
 
 admin.site.register(Event)
 admin.site.register(Profile)
 admin.site.register(Attendance)
 admin.site.register(Tag)
 admin.site.register(TagCategory)
+admin.site.register(InviteRequest)
+admin.site.register(EventInvite)
 
 # Register your models here.
